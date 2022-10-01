cc9751e400176271f9d3dff1efff48ecd38e9fda
Thea Donata Putra <thea@medirecords.com>
2022-10-01 18:27:53 +0700

    Initial setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# laravel-aws-deploy
--- routes/api/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::match(['put', 'patch'], '/{id}', 'UserController@update')->name('users.update');
-});
